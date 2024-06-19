bb5be46900a1bf16e6eb598c7c1ae2821a8c2181
Aryansh Kurmi <arukurmi22@gmail.com>
2024-06-19 23:31:02 +0530

    fix

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,15 +9,15 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
         if (!accessToken) {
             throw new ApiError(401, "Unauthorised request");
         }
-    
+
         const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET,);
-        
+
         const user = await User.findById(decodedToken._id).select("-password -refreshToken");
-    
+
         if (!user) {
             throw new ApiError(401, "Invalid Access Token");
         }
-    
+
         req.user = user;
         next();
     } catch (error) {
@@ -25,6 +25,5 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     }
 
 });
-
 
 export { verifyJWT };
